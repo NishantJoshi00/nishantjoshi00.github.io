f7de711d1c20ac0e7e0d208e370865c36b3951a5
Nishant Joshi <51093026+NishantJoshi00@users.noreply.github.com>
2022-02-16 19:07:51 +0530

    Update script.js

--- hosted/dmust/script.js
+++ hosted/dmust/script.js
@@ -8,6 +8,7 @@ function create_words() {
         // Begin accessing JSON data here
         let data = JSON.parse(this.response);
         if (request.status >= 200 && request.status < 400) {
+            // Remove all the children of the div with id="words"
             let words = document.getElementById("show-list");
             while (words.firstChild) {
                 words.removeChild(words.firstChild);
@@ -22,10 +23,20 @@ function create_words() {
         } else {
             console.log("error");
         }
+
+        document.getElementById("submit").disabled = false;
     }
     request.send();
 }
 
 document.getElementById("submit").addEventListener("click", () => {
+    document.getElementById("submit").disabled = true;
     create_words();
+});
+
+// Event listener for the input field when enter is pressed
+document.getElementById("control").addEventListener("keyup", function (event) {
+    if (event.key == "Enter") {
+        document.getElementById("submit").click();
+    }
 });
