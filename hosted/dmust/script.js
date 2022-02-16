779f2fa6823baae3298fe7895eb1643d5daf4b89
Nishant Joshi <51093026+NishantJoshi00@users.noreply.github.com>
2022-02-16 18:59:24 +0530

    Add files via upload

---
+++ hosted/dmust/script.js
@@ -0,0 +1,27 @@
+function create_words() {
+    let word = document.getElementById("control").value;
+
+    // Create a request to datamuse.com and get the top 10 words
+    let request = new XMLHttpRequest();
+    request.open("GET", "https://api.datamuse.com/words?ml=" + word + "&max=10", true);
+    request.onload = function () {
+        // Begin accessing JSON data here
+        let data = JSON.parse(this.response);
+        if (request.status >= 200 && request.status < 400) {
+            data.forEach(word => {
+                let div = document.createElement("div");
+                div.className = "list-group-item";
+
+                div.innerHTML = word.word;
+                document.getElementById("show-list").appendChild(div);
+            });
+        } else {
+            console.log("error");
+        }
+    }
+    request.send();
+}
+
+document.getElementById("submit").addEventListener("click", () => {
+    create_words();
+});
